9c3169952085bf322077052bbe0134b9fa2cb406
Ziyang Ruan <ruanziyang@gmail.com>
2018-05-15 16:21:54 -0700

    finished the project

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private TextView alsoKnowAsTextview;
+    private TextView ingredientsTextview;
+    private TextView placeOfOriginTextview;
+    private TextView descriptionTextview;
+    private Sandwich sandwich;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -57,6 +65,27 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        alsoKnowAsTextview = findViewById(R.id.also_known_tv);
+        String temp = "";
+        List<String> knownas = sandwich.getAlsoKnownAs();
+        for (String i : knownas) {
+            temp = temp + i + "\n";
+        }
+        alsoKnowAsTextview.setText(temp);
+
+        ingredientsTextview = findViewById(R.id.ingredients_tv);
+        List<String> ingredients = sandwich.getIngredients();
+        temp = "";
+        for (String i : ingredients) {
+            temp = temp + i + "\n";
+        }
+        ingredientsTextview.setText(temp);
+
+        placeOfOriginTextview = findViewById(R.id.origin_tv);
+        placeOfOriginTextview.setText(sandwich.getPlaceOfOrigin());
+
+        descriptionTextview = findViewById(R.id.description_tv);
+        descriptionTextview.setText(sandwich.getDescription());
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,57 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class JsonUtils {
+    private static final String TAG = "JsonUtils";
 
     public static Sandwich parseSandwichJson(String json) {
+        JSONObject jsonBody = null;
+        String mainName;
+        List<String> alsoKnownAs;
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients;
+
+        try {
+            jsonBody = new JSONObject(json);
+            JSONObject nameJsonObject = jsonBody.getJSONObject("name");
+            mainName = nameJsonObject.getString("mainName");
+            alsoKnownAs = makelist(nameJsonObject.getJSONArray("alsoKnownAs"));
+            placeOfOrigin = jsonBody.getString("placeOfOrigin");
+            description = jsonBody.getString("description");
+            image = jsonBody.getString("image");
+            ingredients = makelist(jsonBody.getJSONArray("ingredients"));
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            Log.e(TAG, "Failed to parse JSON", e);
+        }
+
         return null;
+
     }
+
+    private static List<String> makelist(JSONArray arr) {
+        List<String> temp = new LinkedList<>();
+        for (int i = 0; i < arr.length(); i++) {
+            try {
+                temp.add(arr.getString(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return temp;
+    }
+
+
 }
